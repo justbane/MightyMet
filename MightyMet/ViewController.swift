57dc5d282e7b59a8bd68bdb3a23d11e79edb1402
Justin Bane <justbane@justins-macbook-air.local>
2017-01-18 22:18:15 -0800

    Initial Commit

---
+++ MightyMet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MightyMet
+//
+//  Created by Justin Bane on 1/18/17.
+//  Copyright © 2017 Justin Bane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
